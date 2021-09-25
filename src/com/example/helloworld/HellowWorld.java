d4715d42f0bbb4b345e3f295e40c24bbfbfb9f38
Deepthi Jayaprakash <deepthij89@gmail.com>
2021-09-25 20:05:37 +0530

    first commit

---
+++ src/com/example/helloworld/HellowWorld.java
@@ -0,0 +1,30 @@
+package com.example.helloworld;
+
+public class HellowWorld {
+    static void display() {
+        System.out.println("Display function");
+    }
+
+    public static void main(String[] args) {
+        int y = 0;
+        System.out.println("Hello World" + y);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("i");
+        }
+        if () {
+            display();
+        }
+
+    }
+
+
+
+    }
+
+    private static void getShow() {
+        show();
+    }
+
+}
+
